7e7732d4a500274f864a5ff8029afcf268d58969
mzbudi <ronibudianto361@gmail.com>
2023-01-20 17:26:44 +0700

    Setup : Initial Commit, Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto"],
+        radio_canada: ["Radio Canada"],
+      },
+      colors: {
+        primary: "#1E1E1E",
+      },
+    },
+  },
+  plugins: [],
+};
